2000cdf79f1d7932e383c9f748e70482735fb5b8
Monish <116308140+Monishoswal@users.noreply.github.com>
2026-07-28 11:23:24 +0530

    Refactor login_required decorator and wrapper function

--- Login Auth.py
+++ Login Auth.py
@@ -1,11 +1,11 @@
 logged_in = True  # change to True to test
 def login_required(func):
-    def wrapper():
+    def wr():
         if not logged_in:
             print("Access denied. Please log in first.")
         else:
             func()
-    return wrapper
+    return wr
 @login_required
 def dashboard():
     print("Welcome to your dashboard!")
